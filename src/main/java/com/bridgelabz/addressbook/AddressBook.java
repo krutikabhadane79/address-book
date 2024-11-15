33b59db2df7c3786eabeb3c23f305c4db1ebd6d5
krutika bhadane <krutikabhadane79@gmail.com>
2024-11-15 15:42:11 +0530

    Welcome to Address Book

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+
+public class AddressBook
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Address Book" );
+    }
+}
